1d3a49d928f925d9c3dbfc9b7adb8f54e692ac29
Mirko-343 <mirko.becker343@gmail.com>
2024-10-16 12:19:09 -0300

    Upload archivos del parcial

---
+++ main.py
@@ -0,0 +1,88 @@
+﻿# Mirko Becker
+
+# IMPORTANTE: hay algunos prints que se exceden de la cantidad de caracteres por línea pero es porque si los acomodaba
+# con enter para que entraran en la pantalla me daba un error que no tengo conocimiento de cómo solucionar.
+from paquete import funciones
+
+matriz_existencias = funciones.inicializar_matriz(7, 6, 0)
+
+matriz_existencias = funciones.completar_matriz_existencias(matriz_existencias)
+
+# Punto 1 listo
+existencias_pba = [60000, 2500, 300, 750, 999, 800]
+funciones.cargar_existencias(matriz_existencias, "PBA", existencias_pba)
+
+existencias_caba = [500, 555, 780, 654, 200, 801]
+funciones.cargar_existencias(matriz_existencias, "CABA", existencias_caba)
+
+existencias_chubut = [555, 750, 50000, 1200, 2000, 300]
+funciones.cargar_existencias(matriz_existencias, "Chubut", existencias_chubut)
+
+existencias_tucu = [476, 4760, 220, 5478, 52, 302]
+funciones.cargar_existencias(matriz_existencias, "Tucumán", existencias_tucu)
+
+existencias_mend = [2000, 50375, 1, 2000, 1200, 814]
+funciones.cargar_existencias(matriz_existencias, "Mendoza", existencias_mend)
+
+# Punto 2 listo
+print("Punto 2")
+total_existencias_pba = funciones.calcular_total_existencias(matriz_existencias, "PBA")
+print(f"Total de existencias depósito PBA: {total_existencias_pba}")
+
+total_existencias_caba = funciones.calcular_total_existencias(matriz_existencias, "CABA")
+print(f"Total de existencias depósito CABA: {total_existencias_caba}")
+
+total_existencias_chubut = funciones.calcular_total_existencias(matriz_existencias, "Chubut")
+print(f"Total de existencias depósito Chubut: {total_existencias_chubut}")
+
+total_existencias_tucu = funciones.calcular_total_existencias(matriz_existencias, "Tucumán")
+print(f"Total de existencias depósito Tucumán: {total_existencias_tucu}")
+
+total_existencias_mend = funciones.calcular_total_existencias(matriz_existencias, "Mendoza")
+print(f"Total de existencias depósito Mendoza: {total_existencias_mend}")
+
+# Punto 3 listo
+print("\nPunto 3")
+funciones.detectar_escacez(matriz_existencias, 500)
+
+# Punto 4 listo
+print("\nPunto 4")
+funciones.calcular_mayor_existencia(matriz_existencias)
+
+# Punto 5 listo
+print("\nPunto 5")
+lista_valores = [5000, 3500, 4000, 1500, 500, 800]
+
+matriz_recaudaciones = funciones.inicializar_matriz(2, 6, 0)
+matriz_recaudaciones = funciones.completar_matriz_recaudaciones(matriz_recaudaciones)
+
+
+recaudacion_pba = [funciones.calcular_recaudacion_deposito(matriz_existencias, lista_valores, "PBA")]
+matriz_recaudaciones[1][1] = recaudacion_pba
+recaudacion_caba = [funciones.calcular_recaudacion_deposito(matriz_existencias, lista_valores, "CABA")]
+matriz_recaudaciones[1][2] = recaudacion_caba
+recaudacion_chubut = [funciones.calcular_recaudacion_deposito(matriz_existencias, lista_valores, "Chubut")]
+matriz_recaudaciones[1][3] = recaudacion_chubut
+recaudacion_tucu = [funciones.calcular_recaudacion_deposito(matriz_existencias, lista_valores, "Tucumán")]
+matriz_recaudaciones[1][4] = recaudacion_tucu
+recaudacion_mend = [funciones.calcular_recaudacion_deposito(matriz_existencias, lista_valores, "Mendoza")]
+matriz_recaudaciones[1][5] = recaudacion_mend
+
+mayor_recaudacion = funciones.calcular_mayor_recaudacion(matriz_recaudaciones)
+print(f"El depósito con la mayor recaudación fue: {mayor_recaudacion}")
+
+# Punto 6 listo
+print("\nPunto 6")
+funciones.detectar_grandes_existencias(total_existencias_pba, "PBA")
+funciones.detectar_grandes_existencias(total_existencias_caba, "CABA")
+funciones.detectar_grandes_existencias(total_existencias_chubut, "Chubut")
+funciones.detectar_grandes_existencias(total_existencias_tucu, "Tucumán")
+funciones.detectar_grandes_existencias(total_existencias_mend, "Mendoza")
+
+
+
+
+
+
+
+
---
+++ paquete/funciones.py
@@ -0,0 +1,226 @@
+﻿# Mirko Becker
+
+def inicializar_matriz(cantidad_filas : int, cantidad_columnas : int, valor_inicial : any) -> list:
+    ''' Inicializa una matriz en base a los valores recibidos por parámetros.
+        El primer parámetro es la cantidad de filas. El segundo la cantidad de columans y el tercero
+        el valor incial con el que se van a completar las casillas.
+        Devuelve la matriz '''
+        
+    matriz = []
+    
+    for i in range(cantidad_filas):
+        fila = cantidad_columnas * [valor_inicial]
+        matriz += [fila]
+        
+    return matriz
+
+def completar_matriz_existencias(matriz_existencias : list) -> list:
+    ''' Función auxiliar utilizada para completar los valores fijos de la matriz de existencias.
+        Recibe como paráemtero la matriz de existencias vacía '''
+    
+    if type(matriz_existencias) != list:
+        print("El parámetro recibido no es del tipo correcto")
+        return None
+
+    matriz_existencias[0][1] = "PBA"
+    matriz_existencias[0][2] = "CABA"
+    matriz_existencias[0][3] = "Chubut"
+    matriz_existencias[0][4] = "Tucumán"
+    matriz_existencias[0][5] = "Mendoza"
+    
+    matriz_existencias[1][0] = "Autos"
+    matriz_existencias[2][0] = "Muñecas"
+    matriz_existencias[3][0] = "Trenes"
+    matriz_existencias[4][0] = "Peluches"
+    matriz_existencias[5][0] = "Spinners"
+    matriz_existencias[6][0] = "Cartas"
+    
+    return matriz_existencias
+
+def cargar_existencias(matriz_existencias : list, deposito : str, lista_existencias : list) -> list:
+    ''' Permite cargar las existencias de cada depósito en la matriz de existencias.
+        Recibe como primer parámetro la matriz de existencias base con los valores fijos ya cargados.
+        Como segundo parámetro recibe el nombre del depósito para el cual se quieren cargar las existencias
+        Como tercer parámetro recibe la lista con las existencias del depósito correspondiente. Los valores
+        tienen que estar organizados de la siguiente manera
+        1.autos 2.muñecas 3.trenes 4.peluches 5.spinners 6.cartas
+        Devuelve la matriz de existencias con los valores cargados '''
+        
+    if type(matriz_existencias) != list or type(lista_existencias) != list or type(deposito) != str:
+        print("Uno de los parámetros recibidos no es del tipo correcto")
+        return None
+        
+
+
+    for i in range(len(matriz_existencias[0])):
+        if matriz_existencias[0][i] == deposito:
+            matriz_existencias[1][i] = lista_existencias[0]
+            matriz_existencias[2][i] = lista_existencias[1]
+            matriz_existencias[3][i] = lista_existencias[2]
+            matriz_existencias[4][i] = lista_existencias[3]
+            matriz_existencias[5][i] = lista_existencias[4]
+            matriz_existencias[6][i] = lista_existencias[5]
+
+    return matriz_existencias
+            
+def mostrar_matriz(matriz : list) -> None:
+    ''' Función que se encarga de recorrer y mostrar la lista que recibe por parámetro '''
+    
+    if type(matriz) != list:
+        print("El parámetro recibido no es del tipo correcto")
+        return None
+
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            print(f"Fila {i} Columna {j}: {matriz[i][j]}")
+                        
+def calcular_total_existencias(matriz_existencias : list, deposito : str) -> int:
+    ''' Calcula el total de existencias por depósito sumando el total de todos los tipos de juguete
+        Como primer parámetro recibe la matriz de existencias con los valores fijos y varibales ya cargados
+        Como segundo parametro recibe el nombre del depósito para el cual se quiere hacer el cálculo
+        Devuelve un entero con el total de existencias '''
+      
+    if type(matriz_existencias) != list:
+        print("El parámetro recibido no es del tipo correcto")
+        return None
+        
+    total_existencias = 0
+
+    for i in range(len(matriz_existencias[0])):
+        if matriz_existencias[0][i] == deposito:
+            n = len(matriz_existencias)
+            for j in range(n):
+                if j == 0:
+                    continue
+                else:
+                    total_existencias += matriz_existencias[j][i]
+                
+    return total_existencias
+
+def detectar_escacez(matriz_existencias : list, cantidad_minima : int) -> None:
+    ''' Calcula los juguetes que necesitan reposición en cada depósito.
+        Como primer parámetro recibe la matriz de existencias con los valores fijos y variables ya cargados
+        Como segundo parámetro recibe la cantidad mínima de juguetes que puede haber de cada tipo '''
+     
+    if type(matriz_existencias) != list or type(cantidad_minima) != int:
+        print("Uno de los parámetros recibidos no es del tipo correcto")
+        return None
+        
+    for i in range(len(matriz_existencias)):
+        for j in range(len(matriz_existencias[i])):
+            if i != 0 and j != 0:
+                if matriz_existencias[i][j] < cantidad_minima:
+                    # Si acomodo el print para que entre en la pantalla me da un error que no se a qué se debe así que prefiero dejarlo
+                    # así por ahora. Se que no cumple con las reglas de estilo.
+                    print(f"Es necesario reponer {matriz_existencias[i][0]} en el depósito de {matriz_existencias[0][j]}. Hay solamente {matriz_existencias[i][j]}")
+                    
+                else:
+                    continue
+                
+def calcular_mayor_existencia(matriz_existencias : list) -> None:
+    ''' Calcula en qué depósito se encuentra la mayor existencia de cada juguete.
+        Como primer parámetro recibe la matriz de existencias con los valores fijos y variables ya cargados
+        Devuelve un print con la infromación '''
+      
+    if type(matriz_existencias) != list:
+        print("El parámetro recibido no es del tipo correcto")
+        return None
+        
+    for i in range(len(matriz_existencias)):
+        bandera = False
+        mayor_existencia = 0
+        deposito = "a"
+        for j in range(len(matriz_existencias[0])):
+            if i != 0 and j != 0:
+                if bandera == False:
+                    bandera = True
+                    mayor_existencia = matriz_existencias[i][j]
+                    deposito = matriz_existencias[0][j]
+                elif matriz_existencias[i][j] > mayor_existencia:
+                    mayor_existencia = matriz_existencias[i][j]
+                    deposito = matriz_existencias[0][j]
+        if i != 0 and j != 0:
+            print(f"La mayor existencia de {matriz_existencias[i][0]} se encuentra en el depósito de {deposito}. {mayor_existencia} unidades")
+                       
+def calcular_recaudacion_deposito(matriz_existencias : list, lista_valores : list, deposito : str) -> int:
+    ''' Calcula la recaudación total de un depósito.
+        Como primer parámetro recibe la matriz de existencias con los valores fijos y variables ya cargados.
+        Como segundo parámetro recibe una lista con el valor de cada tipo de juguete ordenado de la siguiente manera:
+        1.auto 2.muñeca 3.trenes 4.peluches 5.spinners 6.cartas 
+        Como tercer parámetro recibe el nombre del depósito para el que se quiere hacer el cálculo 
+        Devuelve un entero con la recaudación  total '''
+     
+    if type(matriz_existencias) != list or type(lista_valores) != list or type(deposito) != str:
+        print("Uno de los parámetros recibidos no es del tipo correcto")  
+        return None
+
+    recaudacion_total = 0
+    
+    for i in range(len(matriz_existencias[0])):
+        if matriz_existencias[0][i] == deposito:
+            n = len(matriz_existencias)
+            contador = 0
+            for j in range(n):
+                
+                if j == 0:
+                    continue
+                else:
+                    recaudacion_total += (matriz_existencias[j][i] * lista_valores[contador])
+                    contador += 1
+                
+    return recaudacion_total
+
+def completar_matriz_recaudaciones(matriz_recaudaciones : list) -> list:
+    ''' Completa la matriz de recaudaciones con los valores fijos
+        Devuelve la matriz con los datos cargados '''
+     
+    if type(matriz_recaudaciones) != list:
+        print("Uno de los parámetros recibidos no es del tipo correcto")
+        return None
+        
+    matriz_recaudaciones[0][1] = "PBA"
+    matriz_recaudaciones[0][2] = "CABA"
+    matriz_recaudaciones[0][3] = "Chubut"
+    matriz_recaudaciones[0][4] = "Tucumán"
+    matriz_recaudaciones[0][5] = "Mendoza"
+    
+    return matriz_recaudaciones
+
+def calcular_mayor_recaudacion(matriz_recaudaciones : list) -> str:
+    ''' Calcula el depósito con mayor recaudación.
+        Recibe como primer parámetro la matriz de recaudaciones con los campos fijos y variables ya cargados
+        Devuelve un String con el nombre del depósito que tuvo la mayor recaudación '''
+        
+    if type(matriz_recaudaciones) != list:
+        print("Uno de los parámetros recibidos no es del tipo correcto")
+        return None
+
+    bandera = False
+    for i in range(len(matriz_recaudaciones[0])):
+        if i != 0:
+            if bandera == False:
+                mayor_recaudacion = matriz_recaudaciones[1][i]
+                deposito = matriz_recaudaciones[0][i]
+                bandera = True
+            elif matriz_recaudaciones[1][i] > mayor_recaudacion:
+                mayor_recaudacion = matriz_recaudaciones[1][i]
+                deposito = matriz_recaudaciones[0][i]
+                
+    return deposito
+                
+def detectar_grandes_existencias(existencias_deposito : int, deposito : str) -> None:
+    ''' Calcula los depósitos que acumularon más de 50000 unidades sumando todos los tipos
+        Como primer parámetro recibe el total de las existencias del depósito.
+        Como segundo parámetro recibe el nombre del depósito por el que se quiere filtrar.
+        Devuelve un print informando solamente aquellos depósitos que superaron las 50000 unidades'''
+    
+    if type(existencias_deposito) != int or type(deposito) != str:
+        print("Uno de los parámetros recibidos no es del tipo correcto")
+        return None
+        
+    if existencias_deposito > 50000:
+        print(f"El depósito de {deposito} superó las 50.000 unidades almacenadas entre todos los juguetes")
+        
+
+
+    
